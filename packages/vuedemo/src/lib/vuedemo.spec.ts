97dd40f02cfbce278371e1565753a8038d419cfe
vaishali-ezee <vaishali.patel@ezeetechnosys.com>
2022-05-05 16:39:01 +0530

    feat: add vuedemo library

---
+++ packages/vuedemo/src/lib/vuedemo.spec.ts
@@ -0,0 +1,7 @@
+import { vuedemo } from './vuedemo';
+
+describe('vuedemo', () => {
+  it('should work', () => {
+    expect(vuedemo()).toEqual('vuedemo');
+  });
+});
---
+++ packages/vuedemo/src/lib/vuedemo.ts
@@ -0,0 +1,3 @@
+export function vuedemo(): string {
+  return 'vuedemo';
+}
